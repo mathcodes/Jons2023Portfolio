18a8a64367625c1841c0d200413f9f5509bccd22
mathcodes <jonpchristie@gmail.com>
2023-06-01 11:48:05 -0400

    deploy

---
+++ src/pages/about.jsx
@@ -0,0 +1,112 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import clsx from 'clsx'
+
+import { Container } from '@/components/Container'
+import {
+  GitHubIcon,
+  InstagramIcon,
+  LinkedInIcon,
+  TwitterIcon,
+} from '@/components/SocialIcons'
+import portraitImage from '@/images/avatar.png'
+
+function SocialLink({ className, href, children, icon: Icon }) {
+  return (
+    <li className={clsx(className, 'flex')}>
+      <Link
+        href={href}
+        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+      >
+        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+        <span className="ml-4">{children}</span>
+      </Link>
+    </li>
+  )
+}
+
+function MailIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
+      />
+    </svg>
+  )
+}
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>Jon Christie</title>
+        <meta
+          name="description"
+          content="I’m Jon Christie. I live in Raleigh, North Carolina and work as a Front-End Developer at a small company called 3Advance."
+        />
+      </Head>
+      <Container className="mt-16 sm:mt-32">
+        <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+          <div className="lg:pl-20">
+            <div className="max-w-xs px-2.5 lg:max-w-none">
+              {/* <Image
+                src={portraitImage}
+                alt=""
+                sizes="(min-width: 1024px) 32rem, 20rem"
+                className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+              /> */}
+              <Image
+                src={portraitImage}
+                alt=""
+                sizes="(min-width: 1024px) 32rem, 20rem"
+                className="aspect-circle rotate-3 rounded-2xl bg-none object-cover  "
+              />
+            </div>
+          </div>
+          <div className="lg:order-first lg:row-span-2">
+            <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+              Jon Christie
+            </h1>
+            <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+              <p>
+              Born in South Africa, raised in Philadelphia, and now in North Carolina where I spend my days coding, collaborating, training, and networking to become the best full-stack web developer on your next team!
+              </p>
+              <p>
+                 Front-End Developer with experience working with Javascript, Typescript, React and React Native enhancing and creating websites for clients. As a teacher for 10 years, I also provide guidance and content to clients and the community, bringing clarity to confusion on a daily basis.
+              </p>
+              <p>
+                My ongoing drive to learn and adapt along with my passion for coding, education, gaming, technology, science and green energy have helped me become an irreplaceable asset for the companies and teammates I’ve had the pleasure working with over the years.
+              </p>
+
+            </div>
+          </div>
+          <div className="lg:pl-20">
+            <ul role="list">
+              <SocialLink href="https://twitter.com/jCircle9" icon={TwitterIcon}>
+                Follow on Twitter
+              </SocialLink>
+              <SocialLink href="https://www.instagram.com/jcirclenine/" icon={InstagramIcon} className="mt-4">
+                Follow on Instagram
+              </SocialLink>
+              <SocialLink href="https://github.com/mathcodes" icon={GitHubIcon} className="mt-4">
+                Follow on GitHub
+              </SocialLink>
+              <SocialLink href="https://www.linkedin.com/in/jonpchristie/" icon={LinkedInIcon} className="mt-4">
+                Follow on LinkedIn
+              </SocialLink>
+              <SocialLink
+                href="mailto:jon@jonchristie.net"
+                icon={MailIcon}
+                className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
+              >
+                jon@jonchristie.net
+              </SocialLink>
+            </ul>
+          </div>
+        </div>
+      </Container>
+    </>
+  )
+}
---
+++ src/pages/projects.jsx
@@ -0,0 +1,109 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { Card } from '@/components/Card';
+import { SimpleLayout } from '@/components/SimpleLayout';
+import logoAnimaginary from '@/images/logos/animaginary.svg';
+import logoCosmos from '@/images/logos/cosmos.svg';
+import logoHelioStream from '@/images/logos/helio-stream.svg';
+import logoOpenShuttle from '@/images/logos/open-shuttle.svg';
+import logoPlanetaria from '@/images/logos/planetaria.svg';
+import logoReact from '@/images/logos/react.svg';
+import cfd from '@/images/projects/cfd.png';
+import askSage from '../images/projects/askSage.png';
+import gptfordevs from '@/images/projects/ChatGPT.png';
+// import figma, html, css, react, firebase, GitHub, tailwind
+import Figma from '@/images/logos/figma.svg';
+import HTML from '@/images/logos/html.svg';
+import CSS from '@/images/logos/css.svg';
+import ReactLogo from '@/images/logos/react.svg';
+import Firebase from '@/images/logos/firebase.svg';
+import GitHub from '@/images/logos/GitHub.svg';
+import Tailwind from '@/images/logos/tailwind.svg';
+import OpenAi from '@/images/logos/openaism.svg';
+import Emotion from '@/images/logos/emotion.png';
+import Bootstrap from '@/images/logos/bootstrap.svg';
+import MUI from '@/images/logos/materialui.svg';
+import Python from '@/images/logos/python.svg';
+import Javascript from '@/images/logos/javascript.svg';
+import Git from '@/images/logos/git.svg';
+import NextJS from '@/images/logos/nextJS.svg';
+import Express from '@/images/logos/expressjs.png';
+
+const projects = [
+  {
+    name: 'Content For Developers',
+    description:
+      'This open-source project aims to please users with not just another site full of resources, exercises, and solutions to problems. Contributors are encouraged to upload and share any additional content such as visual diagrams, animated GIFs, notes, charts, or whatever works for them in an effort to provide a more complete learning experience for all utilizing different learning styles.',
+    linkLive: 'http://contentfordevelopers.com',
+    linkGH: 'https://GitHub.com/mathcodes/contentfordevelopers',
+    logo: logoReact,
+    image: cfd,
+    tech: [ReactLogo, Tailwind, HTML, CSS, GitHub, Emotion, MUI, Express],
+  },
+  {
+    name: 'AskSage.ai',
+    description:
+      'New role as the UX/UI and front-end developer for a startup in the AI space. I am responsible for the design, accessibility, and adherence to best practices for the front-end of the application regarding the implementation of AI and machine learning.',
+    linkLive: 'https://asksage.ai',
+    linkGH: '#',
+    image: askSage,
+    tech: [Figma, ReactLogo, Bootstrap, HTML, CSS, Python, Javascript, Git, GitHub],
+  },
+  {
+    name: 'ChatGPT for Developers',
+    description:
+      'Utilize and learn about the different models offered by OpenAI’s GPT-3 API. This project is a simple chat application that allows users to interact with the different models and see the results in real-time.',
+    linkLive: 'https://chatgpt-for-developers.vercel.app/',
+    linkGH: 'https://GitHub.com/mathcodes/chatgpt-for-developers',
+    image: gptfordevs,
+    tech: [OpenAi, ReactLogo, Tailwind, OpenAi, Emotion, HTML, CSS, GitHub, NextJS, Firebase],
+  },
+];
+
+
+
+
+
+export default function Projects() {
+  return (
+    <>
+      <Head>
+        <title>Projects - Jon Christie</title>
+        <meta name="description" content="Some of my most recent and favorite projects." />
+      </Head>
+      <SimpleLayout title="Some of my most recent and favorite projects">
+        <ul
+          role="list"
+          className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
+        >
+          {projects.map((project) => (
+            <Card as="li" key={project.name}>
+              <div className="relative z-10 flex items-center justify-center shadow-md ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:ring-0">
+                <Image src={project.image} alt="" unoptimized />
+              </div>
+              <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
+                {project.linkLive ? (
+                  <Card.Link href={project.linkLive}>{project.name}</Card.Link>
+                ) : (
+                  project.name
+                )}
+              </h2>
+              <div>
+
+              </div>
+              <Card.Description>{project.description}</Card.Description>
+              <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition dark:text-zinc-200">
+                <span className="ml-2 inline-flex flex-shrink-0 items-center rounded-full bg-none px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset border hover:border-white text-black ring-black hover:scale-105 bg-gray-200 hover:bg-[(229, 231, 235)] hover:text-white">
+                  <a href={project.linkGH}>GitHub</a>
+                </span>
+                <span className="ml-2 inline-flex flex-shrink-0 items-center rounded-full bg-green-500 px-1.5 py-0.5 text-xs font-medium text-black ring-1 ring-inset border border-white hover:border-green-400 ring-green-600/20 hover:text-white hover:bg-green-600">
+                  <a href={project.linkLive}>Live</a>
+                </span>
+              </p>
+            </Card>
+          ))}
+        </ul>
+      </SimpleLayout>
+    </>
+  );
+}
