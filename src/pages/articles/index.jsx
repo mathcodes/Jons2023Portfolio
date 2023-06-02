78fc08151f20683993867cd7c0851b8d1571a1b8
mathcodes <jonpchristie@gmail.com>
2023-06-02 10:33:46 -0400

    hn

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -48,7 +48,11 @@ export default function ArticlesIndex({ articles }) {
         title="Check out my articles"
         intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
       >
-        <img src="" alt="clickable image to go to jonchristie.net" />
+        <img
+          src="https://github.com/mathcodes/Jons2023Portfolio/blob/main/src/images/logos/Hashnode.png?raw=true"
+          alt="clickable image to go to jonchristie.net"
+          width="40%"
+        />
 
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
