2f23326515db91c1fd2ba5aa0dae2fbab842a099
Jon Christie <jonpchristie@gmail.com>
2023-06-09 23:11:57 -0400

    skills

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -227,7 +227,7 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer & Trainer.
+            Software Developer & Trainer.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Jon, a software developer and trainer looking for work!
